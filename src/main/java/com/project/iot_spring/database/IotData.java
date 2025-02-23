48ec182488a81396fb15fffc9c748789c4a17cbf
kanthakdominik <kanthakdominik@gmail.com>
2025-02-23 13:51:45 +0100

    added lombok

--- src/main/java/com/project/iot_spring/database/IotData.java
+++ src/main/java/com/project/iot_spring/database/IotData.java
@@ -1,11 +1,17 @@
 package com.project.iot_spring.database;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "iot_data")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class IotData {
 
     @Id
@@ -29,89 +35,4 @@ public class IotData {
 
     private String mode;
 
-    public IotData() {
-    }
-
-    public IotData(int routeId, double latitude, double longitude, LocalDateTime timestamp, int cps, int cpm, double usvPerHour, String mode) {
-        this.routeId = routeId;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.timestamp = timestamp;
-        this.cps = cps;
-        this.cpm = cpm;
-        this.usvPerHour = usvPerHour;
-        this.mode = mode;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getRouteId() {
-        return routeId;
-    }
-
-    public void setRouteId(int routeId) {
-        this.routeId = routeId;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getCps() {
-        return cps;
-    }
-
-    public void setCps(int cps) {
-        this.cps = cps;
-    }
-
-    public int getCpm() {
-        return cpm;
-    }
-
-    public void setCpm(int cpm) {
-        this.cpm = cpm;
-    }
-
-    public double getUsvPerHour() {
-        return usvPerHour;
-    }
-
-    public void setUsvPerHour(double usvPerHour) {
-        this.usvPerHour = usvPerHour;
-    }
-
-    public String getMode() {
-        return mode;
-    }
-
-    public void setMode(String mode) {
-        this.mode = mode;
-    }
 }
