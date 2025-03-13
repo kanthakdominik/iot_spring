bed023acf41af78ff2f317fff403cd6878682569
kanthakdominik <kanthakdominik@gmail.com>
2025-03-13 21:01:05 +0100

    added delete functionalities

--- src/main/java/com/project/iot_spring/database/IotDataRepositoryCustom.java
+++ src/main/java/com/project/iot_spring/database/IotDataRepositoryCustom.java
@@ -4,4 +4,6 @@ import java.util.List;
 
 public interface IotDataRepositoryCustom {
     List<IotData> findByRouteId(int routeId);
+    void deleteAllByRouteId(int routeId);
+    void deleteById(long iotDataId, int routeId);
 }
--- src/main/java/com/project/iot_spring/web/dto/IotDataDTO.java
+++ src/main/java/com/project/iot_spring/web/dto/IotDataDTO.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class IotDataDTO {
 
+    private long id;
     private double latitude;
     private double longitude;
     private double usvPerHour;
